de0916a94085d2a87b6ebd8cf1f6ce1de9a535db
Benno Kohrs <bennokohrs@gmail.com>
2019-11-22 17:44:37 +0100

    Finished MessageJoinCommand and MemberJoinMessage

--- src/main/de/confusingbot/commands/cmds/admincmds/messagecommand/MessageCommand.java
+++ src/main/de/confusingbot/commands/cmds/admincmds/messagecommand/MessageCommand.java
@@ -7,13 +7,16 @@ import net.dv8tion.jda.api.entities.*;
 
 import java.awt.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class MessageCommand implements ServerCommand {
+public class MessageCommand implements ServerCommand
+{
 
-    private String messageKey = "MESSAGE:";
+    private String messageStartKey = "MESSAGE:";
 
     @Override
-    public void performCommand(Member member, TextChannel channel, Message message) {
+    public void performCommand(Member member, TextChannel channel, Message message)
+    {
 
         //- message add welcome [#channel] ([#hexcolor]) ([titleExample]) MESSAGE: Welcome @newMember to the server look at #rule
         //- message remove welcome
@@ -21,12 +24,17 @@ public class MessageCommand implements ServerCommand {
         String[] args = CommandsUtil.messageToArgs(message);
         message.delete().queue();
 
-        if (member.hasPermission(Permission.ADMINISTRATOR)) {
-            if (args.length >= 2) {
-                switch (args[1]) {
+        if (member.hasPermission(Permission.ADMINISTRATOR))
+        {
+            if (args.length >= 2)
+            {
+                switch (args[1])
+                {
                     case "add":
-                        if (args.length >= 3) {
-                            switch (args[2]) {
+                        if (args.length >= 3)
+                        {
+                            switch (args[2])
+                            {
                                 case "welcome":
                                     MessageAddCommand(channel, args, message, MessageManager.welcomeMessageKey);
                                     break;
@@ -38,14 +46,18 @@ public class MessageCommand implements ServerCommand {
                                     MessageManager.embeds.GeneralUsage(channel);
                                     break;
                             }
-                        } else {
+                        }
+                        else
+                        {
                             //Usage
                             MessageManager.embeds.GeneralUsage(channel);
                         }
                         break;
                     case "remove":
-                        if (args.length >= 3) {
-                            switch (args[2]) {
+                        if (args.length >= 3)
+                        {
+                            switch (args[2])
+                            {
                                 case "welcome":
                                     MessageRemoveCommand(channel, args, MessageManager.welcomeMessageKey);
                                     break;
@@ -57,7 +69,9 @@ public class MessageCommand implements ServerCommand {
                                     MessageManager.embeds.GeneralUsage(channel);
                                     break;
                             }
-                        } else {
+                        }
+                        else
+                        {
                             //Usage
                             MessageManager.embeds.GeneralUsage(channel);
                         }
@@ -67,11 +81,15 @@ public class MessageCommand implements ServerCommand {
                         MessageManager.embeds.GeneralUsage(channel);
                         break;
                 }
-            } else {
+            }
+            else
+            {
                 //Usage
                 MessageManager.embeds.GeneralUsage(channel);
             }
-        } else {
+        }
+        else
+        {
             //Error
             MessageManager.embeds.NoPermissionError(channel);
         }
@@ -80,78 +98,111 @@ public class MessageCommand implements ServerCommand {
     //=====================================================================================================================================
     //Commands
     //=====================================================================================================================================
-    private void MessageAddCommand(TextChannel channel, String[] args, Message message, String messageKey) {
+    private void MessageAddCommand(TextChannel channel, String[] args, Message message, String messageKey)
+    {
         Guild guild = channel.getGuild();
-        if (args.length > 3) {
-            if (!MessageManager.sql.MessageExistsInSQL(guild.getIdLong(), messageKey)) {
-                List<TextChannel> mentionedChannel = message.getMentionedChannels();
+        if (args.length > 3)
+        {
+            if (!MessageManager.sql.MessageExistsInSQL(guild.getIdLong(), messageKey))
+            {
 
-                //Get channel where to send the message
-                TextChannel messageChannel = mentionedChannel.get(0);
-                mentionedChannel.remove(0);
+                List<TextChannel> mentionedChannelDiscord = message.getMentionedChannels();
 
-                List<Role> mentionedRoles = message.getMentionedRoles();
-                List<User> mentionedUser = message.getMentionedUsers();
+                if (mentionedChannelDiscord.size() > 0 && mentionedChannelDiscord != null)
+                {
+                    //Get channel where to send the message
+                    TextChannel messageChannel = mentionedChannelDiscord.get(0);
+                    List<TextChannel> mentionedChannel = mentionedChannelDiscord.stream().collect(Collectors.toList());
+                    mentionedChannel.remove(0);
 
-                if (args[3].contains(messageChannel.getName())) {
-                    //Get Color
-                    String defaultColor = "#ffa500";
-                    String color = defaultColor;
-                    if (args[4].startsWith("#") && ColorExists(args[4])) {
-                        color = args[4];
-                    }
+                    List<Role> mentionedRoles = message.getMentionedRoles();
+                    List<User> mentionedUser = message.getMentionedUsers();
 
-                    boolean hasChosenNewColor = !color.equals(defaultColor);
-                    if ((args.length >= 4 && !hasChosenNewColor) || args.length >= 5) {
-                        int startIndex = hasChosenNewColor ? 5 : 4;
-
-                        String wholeMessage = getWholeMessage(args, startIndex);
-                        String title = "";
-                        String m = "";
-
-                        if (wholeMessage.contains(messageKey)) {
-                            String[] messageAndTitle = wholeMessage.split(messageKey);
-                            title = messageAndTitle[0];
-                            m = messageAndTitle[1];
+                    if (args[3].contains(messageChannel.getName()))
+                    {
+                        //Get Color
+                        String defaultColor = "#ffa500";
+                        String color = defaultColor;
+                        if (args[4].startsWith("#") && ColorExists(args[4]))
+                        {
+                            color = args[4];
                         }
 
-                        //SQL
-                        MessageManager.sql.MessageAddToSQL(guild.getIdLong(),
-                                messageChannel.getIdLong(),
-                                color,
-                                messageKey,
-                                title,
-                                getMentionableMessage(m, mentionedChannel, mentionedRoles, mentionedUser));
+                        boolean hasChosenNewColor = !color.equals(defaultColor);
+                        if ((args.length >= 4 && !hasChosenNewColor) || args.length >= 5)
+                        {
+                            int startIndex = hasChosenNewColor ? 5 : 4;
 
-                        //Message
-                        MessageManager.embeds.SuccessfullyAddedMessage(channel, messageKey);
-                    } else {
-                        //Error
-                        MessageManager.embeds.NoMessageDefinedError(channel, messageKey);
+                            String wholeMessage = getWholeMessage(args, startIndex);
+                            String title = "";
+                            String shownMessage = "";
+
+                            if (wholeMessage.contains(messageStartKey))
+                            {
+                                String[] messageAndTitle = wholeMessage.split(messageStartKey);
+                                title = messageAndTitle[0];
+                                shownMessage = messageAndTitle[1];
+                            }
+                            else
+                            {
+                                shownMessage = wholeMessage;
+                            }
+
+                            //SQL
+                            MessageManager.sql.MessageAddToSQL(guild.getIdLong(),
+                                    messageChannel.getIdLong(),
+                                    color,
+                                    messageKey,
+                                    title,
+                                    getMentionableMessage(shownMessage, mentionedChannel, mentionedRoles, mentionedUser));
+
+                            //Message
+                            MessageManager.embeds.SuccessfullyAddedMessage(channel, messageKey);
+                        }
+                        else
+                        {
+                            //Error
+                            MessageManager.embeds.NoMessageDefinedError(channel, messageKey);
+                        }
                     }
-                } else {
-                    //Error
-                    MessageManager.embeds.NoMessageChannelMentionedError(channel, messageKey);
+                    else
+                    {
+                        //Error
+                        MessageManager.embeds.NoMessageChannelMentionedError(channel, messageKey);
+                    }
                 }
-            } else {
-                //Error
-                MessageManager.embeds.MessageAlreadyExistsError(channel, messageKey);
+                else
+                {
+                    //Error
+                    MessageManager.embeds.MessageAlreadyExistsError(channel, messageKey);
+                }
             }
-        } else {
+            else
+            {
+                //Error
+                MessageManager.embeds.NoMessageChannelMentionedError(channel, messageKey);
+            }
+        }
+        else
+        {
             //Usage
             MessageManager.embeds.MessageAddUsage(channel);
         }
     }
 
-    private void MessageRemoveCommand(TextChannel channel, String[] args, String messageKey) {
+    private void MessageRemoveCommand(TextChannel channel, String[] args, String messageKey)
+    {
         Guild guild = channel.getGuild();
-        if (args.length == 3) {
+        if (args.length == 3)
+        {
             //SQL
             MessageManager.sql.MessageRemoveFromSQL(guild.getIdLong(), messageKey);
 
             //Message
             MessageManager.embeds.SuccessfullyRemovedMessage(channel, messageKey);
-        } else {
+        }
+        else
+        {
             //Usage
             MessageManager.embeds.MessageAddUsage(channel);
         }
@@ -160,41 +211,54 @@ public class MessageCommand implements ServerCommand {
     //=====================================================================================================================================
     //Helper
     //=====================================================================================================================================
-    private boolean ColorExists(String hexColor) {
-        try {
+    private boolean ColorExists(String hexColor)
+    {
+        try
+        {
             Color color = Color.decode(hexColor);
             return true;
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e)
+        {
             e.printStackTrace();
         }
         return false;
     }
 
-    private String getWholeMessage(String[] args, int startIndex) {
+    private String getWholeMessage(String[] args, int startIndex)
+    {
         StringBuilder builder = new StringBuilder();
-        for (int i = startIndex; i < args.length; i++) {
+        for (int i = startIndex; i < args.length; i++)
+        {
             builder.append(args[i] + " ");
         }
         String wholeMessage = builder.toString();
         return wholeMessage.trim();
     }
 
-    private String getMentionableMessage(String message, List<TextChannel> mentionedChannel, List<Role> mentionedRoles, List<User> mentionedUsers) {
+    private String getMentionableMessage(String message, List<TextChannel> mentionedChannel, List<Role> mentionedRoles, List<User> mentionedUsers)
+    {
         String mentionableMessage = "";
         StringBuilder builder = new StringBuilder();
         String[] words = message.split(" ");
 
-        for (String word : words) {
-            for (TextChannel channel : mentionedChannel) {
-                word = channel.getAsMention();
+        for (String word : words)
+        {
+            for (TextChannel channel : mentionedChannel)
+            {
+                if (word.contains(channel.getName()))
+                    word = channel.getAsMention();
             }
 
-            for (Role role : mentionedRoles) {
-                word = role.getAsMention();
+            for (Role role : mentionedRoles)
+            {
+                if (word.contains(role.getName()))
+                    word = role.getAsMention();
             }
 
-            for (User user : mentionedUsers) {
-                word = user.getAsMention();
+            for (User user : mentionedUsers)
+            {
+                if (word.contains(user.getName()))
+                    word = user.getAsMention();
             }
 
             builder.append(word + " ");
