c77cfefdebfa495f57769fcc8248082f925c883d
Benno Kohrs <bennokohrs@gmail.com>
2019-12-20 15:00:46 +0100

    Added onMemberLeaveListener in the ReactRoleCommand -> if a member leave a guild the reactions on the ReeactRoleMessages will be removed

--- src/main/de/confusingbot/commands/cmds/admincmds/reactrolescommand/ReactRolesListener.java
+++ src/main/de/confusingbot/commands/cmds/admincmds/reactrolescommand/ReactRolesListener.java
@@ -1,15 +1,17 @@
 package main.de.confusingbot.commands.cmds.admincmds.reactrolescommand;
 
+import main.de.confusingbot.commands.cmds.admincmds.acceptrulecommand.AcceptRuleManager;
+import main.de.confusingbot.commands.help.CommandsUtil;
 import main.de.confusingbot.manage.sql.LiteSQL;
-import net.dv8tion.jda.api.entities.ChannelType;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.exceptions.HierarchyException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class ReactRolesListener
 {
@@ -17,102 +19,143 @@ public class ReactRolesListener
     public void onReactionAdd(MessageReactionAddEvent event)
     {
         Guild guild = event.getGuild();
-            if (event.getChannelType() == ChannelType.TEXT)
+        if (event.getChannelType() == ChannelType.TEXT)
+        {
+            long guildid = event.getGuild().getIdLong();
+            long channelid = event.getChannel().getIdLong();
+            long messageid = event.getMessageIdLong();
+            if (ReactRoleManager.sql.containsMessageID(event.getGuild().getIdLong(), messageid))
             {
-                long guildid = event.getGuild().getIdLong();
-                long channelid = event.getChannel().getIdLong();
-                long messageid = event.getMessageIdLong();
-                if (ReactRoleManager.sql.containsMessageID(event.getGuild().getIdLong(), messageid))
+                if (!event.getUser().isBot())
                 {
-                    if (!event.getUser().isBot())
+                    String emote = "";
+
+                    if (event.getReactionEmote().isEmoji())
                     {
-                        String emote = "";
+                        emote = event.getReactionEmote().getEmoji();
+                    }
+                    else
+                    {
+                        emote = event.getReactionEmote().getId();
+                    }
 
-                        if (event.getReactionEmote().isEmoji())
+                    long roleid = ReactRoleManager.sql.GetRoleIdFromSQL(guildid, channelid, messageid, emote);
+                    Role role = guild.getRoleById(roleid);
+
+                    if (role != null)
+                    {
+
+                        try
                         {
-                            emote = event.getReactionEmote().getEmoji();
+                            //Add Role to member
+                            guild.addRoleToMember(event.getMember(), guild.getRoleById(roleid)).queue();
                         }
-                        else
-                        {
-                            emote = event.getReactionEmote().getId();
-                        }
-
-                        long roleid = ReactRoleManager.sql.GetRoleIdFromSQL(guildid, channelid, messageid, emote);
-                        Role role = guild.getRoleById(roleid);
-
-                        if(role != null){
-
-                            try
-                            {
-                                //Add Role to member
-                                guild.addRoleToMember(event.getMember(), guild.getRoleById(roleid)).queue();
-                            } catch (HierarchyException e)
-                            {
-                                //Error
-                                ReactRoleManager.embeds.BotHasNoPermissionToAssignRole(event.getTextChannel(), role);
-                            }
-                        }
-                        else
+                        catch (HierarchyException e)
                         {
                             //Error
-                            ReactRoleManager.embeds.RoleDoesNotExistError(event.getTextChannel(), roleid);
-
-                            //SQL
-                            ReactRoleManager.sql.removeFromSQL(guildid, channelid, messageid, emote, roleid);
-
+                            ReactRoleManager.embeds.BotHasNoPermissionToAssignRole(event.getTextChannel(), role);
                         }
+                    }
+                    else
+                    {
+                        //Error
+                        ReactRoleManager.embeds.RoleDoesNotExistError(event.getTextChannel(), roleid);
+
+                        //SQL
+                        ReactRoleManager.sql.removeFromSQL(guildid, channelid, messageid, emote, roleid);
+
                     }
                 }
             }
-
-
+        }
     }
 
     public void onReactionRemove(MessageReactionRemoveEvent event)
     {
         Guild guild = event.getGuild();
-            if (event.getChannelType() == ChannelType.TEXT)
+        if (event.getChannelType() == ChannelType.TEXT)
+        {
+            long guildid = event.getGuild().getIdLong();
+            long channelid = event.getChannel().getIdLong();
+            long messageid = event.getMessageIdLong();
+            if (ReactRoleManager.sql.containsMessageID(event.getGuild().getIdLong(), messageid))
             {
-                long guildid = event.getGuild().getIdLong();
-                long channelid = event.getChannel().getIdLong();
-                long messageid = event.getMessageIdLong();
-                if (ReactRoleManager.sql.containsMessageID(event.getGuild().getIdLong(), messageid))
+                if (!event.getUser().isBot())
                 {
-                    if (!event.getUser().isBot())
+                    String emote = "";
+
+                    if (event.getReactionEmote().isEmoji())
                     {
-                        String emote = "";
+                        emote = event.getReactionEmote().getEmoji();
+                    }
+                    else
+                    {
+                        emote = event.getReactionEmote().getId();
+                    }
 
-                        if (event.getReactionEmote().isEmoji())
+                    long roleid = ReactRoleManager.sql.GetRoleIdFromSQL(guildid, channelid, messageid, emote);
+                    Role role = guild.getRoleById(roleid);
+
+                    if (role != null)
+                    {
+                        try
                         {
-                            emote = event.getReactionEmote().getEmoji();
+                            //Remove Role from member
+                            guild.removeRoleFromMember(event.getMember(), guild.getRoleById(roleid)).queue();
                         }
-                        else
+                        catch (HierarchyException e)
                         {
-                            emote = event.getReactionEmote().getId();
+                            //Error
+                            ReactRoleManager.embeds.BotHasNoPermissionToAssignRole(event.getTextChannel(), role);
                         }
+                    }
+                    else
+                    {
+                        //Error
+                        ReactRoleManager.embeds.RoleDoesNotExistError(event.getTextChannel(), roleid);
 
-                        long roleid = ReactRoleManager.sql.GetRoleIdFromSQL(guildid, channelid, messageid, emote);
-                        Role role = guild.getRoleById(roleid);
-
-                        if(role != null) {
-                            try {
-                                //Remove Role from member
-                                guild.removeRoleFromMember(event.getMember(), guild.getRoleById(roleid)).queue();
-                            } catch (HierarchyException e) {
-                                //Error
-                                ReactRoleManager.embeds.BotHasNoPermissionToAssignRole(event.getTextChannel(), role);
-                            }
-                        }
-                         else
-                            {
-                                //Error
-                                ReactRoleManager.embeds.RoleDoesNotExistError(event.getTextChannel(), roleid);
-
-                                //SQL
-                                ReactRoleManager.sql.removeFromSQL(guildid, channelid, messageid, emote, roleid);
-                            }
+                        //SQL
+                        ReactRoleManager.sql.removeFromSQL(guildid, channelid, messageid, emote, roleid);
                     }
                 }
             }
+        }
+    }
+
+    public void onMemberLeaveListener(GuildMemberLeaveEvent event)
+    {
+        Guild guild = event.getGuild();
+        ResultSet set = ReactRoleManager.sql.GetReactRolesResultSet(guild.getIdLong());
+
+        try
+        {
+            while (set.next())
+            {
+
+                long channelID = set.getLong("channelid");
+                long messageID = set.getLong("messageid");
+
+                TextChannel channel = guild.getTextChannelById(channelID);
+                Message message = CommandsUtil.getLatestesMessageByID(channel, messageID);
+
+                if (message != null && channel != null)
+                {
+                    List<MessageReaction> reactions = message.getReactions();
+
+                    for (MessageReaction reaction : reactions)
+                    {
+                        User user = event.getUser();
+                        if (user != null)
+                        {
+                            message.removeReaction(reaction.getReactionEmote().getEmoji(), user).queue();
+                        }
+                    }
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
