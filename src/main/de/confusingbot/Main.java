35644d96a3fbefe61b0845ec210ec4f9811729cf
Benno Kohrs <bennokohrs@gmail.com>
2020-01-26 19:55:36 +0100

    Counted to 0.0.08

--- src/main/de/confusingbot/Main.java
+++ src/main/de/confusingbot/Main.java
@@ -32,8 +32,8 @@ public class Main
     //ConfusingTestBot Token: NjQ3MTQ3NDkyOTQwNTc4ODI2.Xe0BKw.le3xRsP2stezvJbhXnr8gIKtaSQ
     //ConfusingBot Token: NjM4NzYwNDYwODEyMDI1ODY2.XhjmyQ.jjGdRKCy8prSzczjp-8UfvPtTgM
     private String token = "NjQ3MTQ3NDkyOTQwNTc4ODI2.Xe0BKw.le3xRsP2stezvJbhXnr8gIKtaSQ";
-    public static String version = "0.0.07";
-    public static long linesOfCode = 12589;
+    public static String version = "0.0.08";
+    public static long linesOfCode = 13179;
     public static String prefix = "- ";
     public static LocalDateTime botStartTime = LocalDateTime.now();
 
